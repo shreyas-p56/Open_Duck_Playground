ffaab4427e82533f2ea81ed0eeb463918b4ba58b
shreyas-p56 <shreyasp958@gmail.com>
2026-08-30 01:58:14 +0530

    reduce num_envs to suit RTX 4060

--- playground/common/runner.py
+++ playground/common/runner.py
@@ -89,6 +89,7 @@ class BaseRunner(ABC):
         )  # TODO
         self.ppo_training_params = dict(self.ppo_params)
         # self.ppo_training_params["num_timesteps"] = 150000000 * 20
+        self.ppo_training_params["num_envs"] = 4096
         
 
         if "network_factory" in self.ppo_params:
